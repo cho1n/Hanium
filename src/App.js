66691f02d5d6765a67843400dc5ab9c3110a8032
cho1n <cjaewon0217@naver.com>
2023-05-16 00:28:13 +0900

    2023_05_16_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					å
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+		<div>
+			<h2>Let's get started!</h2>
 		</div>
 	);
 }
@@ -26,3 +12,6 @@ export default App;
 
 // let : 변하는 변수
 // const : 변하지 않는 변수
+// 컴포넌트는 유저 인터페이스를 갖추기 위한 Html, css, 그리고 로직 구현을 위한 자바 스크립트의 집합체
+// 리액트는 컴포넌트다.
+// 선언적 접근요소 
